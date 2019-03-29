24145b7038713f8c2917f04d13061c8761d187fe
Arturo <ing.arturoescamilla@outlook.com>
2019-03-29 16:53:24 -0600

    Etapa1.1

---
+++ Etapa1/Entidades/Escuela.cs
@@ -0,0 +1,36 @@
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        string nombre;
+        public string Nombre
+        {   // Cuando alguien pregunte por el nombre.
+            get { return "Copia: " + nombre; } // return - lo que va a devolver - la variable nombre.  
+            set { nombre = value.ToUpper(); }// Asignar un valor a esta propiedad, se asigna nombre a value.
+        } // value - palabra clave
+
+        // ToUpper(); => Convert to Mayus
+        // shift + alt + f => Format Document
+
+        public int AñoDeCreacion { get; set; } // prop - atajo para crear una propiedad.
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        private int myVar;
+
+        // Todo objeto para que se pueda crear.
+        // Cuenta con el método Constructor.
+        // El constructor es un metodo que.
+        // Nos ayuda a crear una instancia del objeto.
+
+        /* METODO CONSTRUCTOR
+        public Escuela(string nombre, int año)
+        {
+            this.nombre = nombre;
+            AñoDeCreacion = año;
+        }*/
+
+        // Atajo, escribirlo de manera mas corta:
+        // Igualación por tuplas
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año);
+    }
+}
